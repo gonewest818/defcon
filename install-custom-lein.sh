1ffb7e09a416ff3be86b1b6ec818f62a3114aed7
Neil Okamoto <neil.okamoto@gmail.com>
2017-08-24 17:29:58 -0700

    use custom leiningen for unattended deploys

---
+++ install-custom-lein.sh
@@ -0,0 +1,17 @@
+#!/bin/bash -ex
+
+# we're deploying with this custom build of leiningen until
+# our pull request supporting unattended signatures is merged.
+# (https://github.com/technomancy/leiningen/pull/2279)
+
+
+mkdir -p /opt
+cd /opt
+git clone https://github.com/gonewest818/leiningen.git
+cd leiningen
+git checkout unattended-signatures
+cd leiningen-core
+lein bootstrap
+cd ..
+bin/lein compile
+
